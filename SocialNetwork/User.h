fd146dac1074dc7949ef2342ba62f9a0322f5036
Infifu <iliiniliya2008@gmail.com>
2024-11-17 15:36:36 +0200

    User

--- SocialNetwork/User.h
+++ SocialNetwork/User.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+#include "DeviceList.h"
+#include "Device.h"
+
+class User
+{
+private:
+	unsigned int _id;
+	unsigned int _age;
+	std::string _username;
+	DevicesList _list;
+public:
+	void init(unsigned int id, std::string username, unsigned int age);
+	void clear();
+	unsigned int getID() const;
+	std::string getUserName() const;
+	unsigned int getAge() const;
+	DevicesList& getDevices();
+	void addDevice(Device newDevice);
+	bool checkIfDevicesAreOn() const;
+};
