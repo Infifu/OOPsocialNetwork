8d0aff01602ddcb06e161cacd2d4269adeb34484
Infifu <iliiniliya2008@gmail.com>
2024-11-18 01:23:41 +0200

    Little bit errors to fix

---
+++ SocialNetwork/SocialNetwork.cpp
@@ -0,0 +1,63 @@
+#include "SocialNetwork.h"
+
+void SocialNetwork::init(std::string networkName, int minAge)
+{
+	_networkName = networkName;
+	_minAge = minAge;
+	_profiles.init();
+}
+
+void SocialNetwork::clear()
+{
+	_networkName.clear();
+	_minAge = 0;
+}
+
+std::string SocialNetwork::getNetworkName()
+{
+	return _networkName;
+}
+
+int SocialNetwork::getMinAge()
+{
+	return _minAge;
+}
+
+bool SocialNetwork::addProfile(Profile profile_to_add)
+{
+	if (profile_to_add.getOwner().getAge() < _minAge)
+		return false;
+	_profiles.add(profile_to_add);
+	
+}
+
+std::string SocialNetwork::getWindowsDevices()
+{
+	ProfileNode* profilecurr = _profiles.get_first();
+	std::string deviceANDid = "";
+	std::string  deviceid = "";
+	DeviceNode* deviceCurr = nullptr;
+	std::string devicesString = "";
+	int devicesStringLength = 0;
+	while (profilecurr)
+	{
+		deviceCurr = profilecurr->get_data().getOwner().getDevices().get_first();
+		while (deviceCurr)
+		{
+			if (deviceCurr->get_data().getOS().find("Windows") != std::string::npos)
+			{
+				deviceid = std::to_string(deviceCurr->get_data().getID());
+				deviceANDid = deviceANDid + "[" + deviceid + ", " + deviceCurr->get_data().getOS() + "]";
+				devicesString = devicesString + deviceANDid + ", ";
+				deviceid.clear();
+				devicesString.clear();
+			}
+			deviceCurr = deviceCurr->get_next();
+		}
+		profilecurr = profilecurr->get_next();
+	}
+	devicesStringLength = devicesString.length();
+	if (devicesStringLength > 1)
+		devicesString.resize(devicesStringLength - 1);
+	return devicesString;
+}
---
+++ SocialNetwork/SocialNetwork.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include "Profile.h"
+#include "ProfileList.h"
+
+class SocialNetwork
+{
+private:
+	std::string _networkName;
+	int _minAge;
+	ProfileList _profiles;
+public:
+	void init(std::string networkName, int minAge);
+	void clear();
+	std::string getNetworkName();
+	int getMinAge();
+	bool addProfile(Profile profile_to_add);
+	std::string getWindowsDevices();
+};
